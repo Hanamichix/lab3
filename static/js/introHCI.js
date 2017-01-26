e5a450270660c89fd70fe115d6dea5022c9fd4a2
Pei Xu <ipeggy.hsu@gmail.com>
2017-01-26 10:28:45 -0800

    hello

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -31,7 +31,9 @@ function projectClick(e) { 
     if (description.length == 0) {
 		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-    	$(".project-description").fadeOut()
+    	$(".project-description").animate({
+    		height: "toggle"
+    	});
 //       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
     }
 }
